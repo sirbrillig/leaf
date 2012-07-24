e97cf00315779438a2355f305278e53f7fa6f870
Payton Swick <payton@foolord.com>
2012-07-24 13:55:58 -0400

    Editor now uses my own subclass.

--- lib/game.rb
+++ lib/game.rb
@@ -29,54 +29,55 @@ module Leaf
     end
 	end # Game
 
+  class LevelEditor < Chingu::GameStates::Edit
+    def setup
+      super
+      editable_game_objects.each { |object| object.show! if object.respond_to? :show! }
+      on_input(:n, :toggle_snap_to_grid)
+    end
+
+    def toggle_snap_to_grid
+      @snap_to_grid = !@snap_to_grid
+    end
+
+    def draw_toolbar_objects
+      x = 20
+      y = 60
+      @classes.each do |klass|
+        puts "Creating a #{klass}"  if @debug
+
+        # We initialize x,y,zorder,rotation_center after creation
+        # so they're not overwritten by the class initialize/setup or simular
+        begin
+          game_object = klass.create(:paused => true)
+          game_object.x = x + 30
+          game_object.y = y
+          game_object.options[:toolbar] = true
+          game_object.rotation_center = :center_center
+          game_object.alpha = 255
+
+          # Scale down object to fit our toolbar
+          if game_object.image
+            Text.create("#{klass.name.split('::').last.to_s[0..9]}\n#{game_object.width.to_i}x#{game_object.height.to_i}", :size => 18, :x=>x-16, :y=>y+28, :max_width => 100, :rotation_center => :top_left, :align => :center, :factor => 1)
+            game_object.factor = 0.7
+            x += 80
+          else
+            puts "Skipping #{klass} - no image" if @debug
+            game_object.destroy
+          end
+        rescue
+          puts "Couldn't use #{klass} in editor: #{$!}"
+        end
+      end        
+    end
+  end
+
+
 end # Leaf
 
 module Chingu::Traits::BoundingCircle
   def radius=(rad)
     @cached_radius = rad
-  end
-end
-
-class Chingu::GameStates::Edit
-  alias_method :old_setup, :setup
-  def setup
-    old_setup
-    editable_game_objects.each { |object| object.show! if object.respond_to? :show! }
-    on_input(:n, :toggle_snap_to_grid)
-  end
-
-  def toggle_snap_to_grid
-    @snap_to_grid = !@snap_to_grid
-  end
-
-  def draw_toolbar_objects
-    x = 20
-    y = 60
-    @classes.each do |klass|
-      puts "Creating a #{klass}"  if @debug
-
-      # We initialize x,y,zorder,rotation_center after creation
-      # so they're not overwritten by the class initialize/setup or simular
-      begin
-        game_object = klass.create(:paused => true)
-        game_object.x = x + 10
-        game_object.y = y
-        game_object.options[:toolbar] = true
-        game_object.rotation_center = :center_center
-
-        # Scale down object to fit our toolbar
-        if game_object.image
-          Text.create("#{klass.name.split('::').last.to_s[0..9]}\n#{game_object.width.to_i}x#{game_object.height.to_i}", :size => 12, :x=>x-16, :y=>y+18, :max_width => 55, :rotation_center => :top_left, :align => :center, :factor => 1)
-          game_object.size = @toolbar_icon_size
-          x += 50
-        else
-          puts "Skipping #{klass} - no image" if @debug
-          game_object.destroy
-        end
-      rescue
-        puts "Couldn't use #{klass} in editor: #{$!}"
-      end
-    end        
   end
 end
 
